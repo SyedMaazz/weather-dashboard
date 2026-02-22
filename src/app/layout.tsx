20cb0b48fb77afc510c816aed825de256e3cab3f
syedmaaz <maazdeys@gmail.com>
2026-02-22 21:35:04 +0530

    feat: change name of website to nothing weather

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 
+export const metadata = {
+  title: "Nothing Weather",
+  description: "Pixel weather dashboard",
+};
+
 export default function RootLayout({
   children,
 }: {
